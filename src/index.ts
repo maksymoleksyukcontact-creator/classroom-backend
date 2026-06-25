3745e8ff7d6471f962b2d224648a5e83f048c407
maksim <maksim@topdevs.org>
2026-06-25 15:40:59 +0300

    init backend server

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+	res.json({ message: "Hello from Classroom backend!" });
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running at http://localhost:${PORT}/`);
+});
+
