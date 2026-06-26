32ef1fabf59082ae43e683e4803bf3c5f202a601
maksim <maksim@topdevs.org>
2026-06-26 18:24:51 +0300

    add subject types

--- src/db/schema/app.ts
+++ src/db/schema/app.ts
@@ -35,5 +35,7 @@ export const subjectsRelations = relations(subjects, ({ one }) => ({
 }));
 
 export type Department = typeof departments.$inferSelect;
-
 export type NewDepartment = typeof departments.$inferInsert;
+
+export type Subject = typeof subjects.$inferSelect;
+export type NewSubject = typeof subjects.$inferInsert;
